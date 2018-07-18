fd8cd1db9ac8fce793f710ca2a0415f1c254622b
jsay <jsay.site@gmail.com>
2018-07-18 22:47:26 +0200

    Add some general config in init.el

--- init.el
+++ init.el
@@ -1,8 +1,35 @@
+(prefer-coding-system 'utf-8)
+(modify-coding-system-alist 'file "\\.tex\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.org\\'" 'utf-8)
+(setq org-export-latex-coding-system 'utf-8)
+(setq org-export-coding-system 'utf-8)
 
-(setq default-directory "/home/jsay/")
-(setq load-path (cons "/hone/jsay/emacs-config" load-path))
-(package-initialize)
-(setq package-archives '(("gnu"      . "http://elpa.gnu.org/packages/")
-                         ("marmalade". "http://marmalade-repo.org/packages/")
-                         ("melpa"    . "http://melpa.org/packages/")
-                         ("org"      . "http://orgmode.org/elpa/")))
+(load-library "iso-transl")
+
+(add-to-list 'custom-theme-load-path "/home/jsay/.emacs.d/themes/")
+(load-theme 'cyberpunk t)
+
+(set-face-attribute 'default nil :height 150)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(blink-cursor-mode -1)
+
+(setq split-width-threshold 150)
+
+(setq inhibit-startup-message t)
+(find-file "Main.org")
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+(toggle-fullscreen)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
