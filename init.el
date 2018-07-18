b2179a011db55ac56660360382cb8bff9b200bca
jsay <jsay.site@gmail.com>
2018-07-18 22:14:47 +0200

    stage init.el

---
+++ init.el
@@ -0,0 +1,8 @@
+
+(setq default-directory "/home/jsay/")
+(setq load-path (cons "/hone/jsay/emacs-config" load-path))
+(package-initialize)
+(setq package-archives '(("gnu"      . "http://elpa.gnu.org/packages/")
+                         ("marmalade". "http://marmalade-repo.org/packages/")
+                         ("melpa"    . "http://melpa.org/packages/")
+                         ("org"      . "http://orgmode.org/elpa/")))
