5bcdde5ba4cd28fd5d437e1c4c00ae0cec51520c
jsay <jsay.site@gmail.com>
2019-01-03 16:13:56 +0100

    modify moody and add tabbar

--- init.el
+++ init.el
@@ -15,18 +15,12 @@
        (load-theme 'cyberpunk t)
        (let ((line (face-attribute 'mode-line :underline)))
 	 (set-face-attribute 'org-level-1        nil :foreground "#f9f2d9")
-	 (set-face-attribute 'mode-line          nil :overline   line)
-	 (set-face-attribute 'mode-line-inactive nil :overline   line)
-	 (set-face-attribute 'mode-line-inactive nil :underline  line)
+	 (set-face-attribute 'mode-line          nil :overline   nil)
+	 (set-face-attribute 'mode-line-inactive nil :overline   nil)
+	 (set-face-attribute 'mode-line-inactive nil :underline  nil)
 	 (set-face-attribute 'mode-line          nil :box        nil)
 	 (set-face-attribute 'mode-line-inactive nil :box        nil)
 	 (set-face-attribute 'mode-line-inactive nil :background "#f9f2d9")))))
-
-(use-package moody
-  :config
-  (setq x-underline-at-descent-line t)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode))
 
   (set-face-attribute 'default nil :height 175)
 
@@ -66,6 +60,41 @@
   (setq x-underline-at-descent-line t)
   (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode))
+
+(require 'tabbar)
+(customize-set-variable 'tabbar-background-color "gray20")
+(customize-set-variable 'tabbar-separator '(0.5))
+(customize-set-variable 'tabbar-use-images nil)
+(tabbar-mode 1)
+(define-key global-map [(alt j)] 'tabbar-backward)
+(define-key global-map [(alt k)] 'tabbar-forward)
+
+;; Colors
+(set-face-attribute 'tabbar-default nil
+        :background "gray20" :foreground 
+        "gray60" :distant-foreground "gray50"
+        :family "Helvetica Neue" :box nil)
+(set-face-attribute 'tabbar-unselected nil
+        :background "gray80" :foreground "black" :box nil)
+(set-face-attribute 'tabbar-modified nil
+        :foreground "red4" :box nil
+        :inherit 'tabbar-unselected)
+(set-face-attribute 'tabbar-selected nil
+        :background "#4090c0" :foreground "white" :box nil)
+(set-face-attribute 'tabbar-selected-modified nil
+        :inherit 'tabbar-selected :foreground "GoldenRod2" :box nil)
+(set-face-attribute 'tabbar-button nil
+        :box nil)
+
+(require 'powerline)
+(defvar my/tabbar-height 20)
+(defvar my/tabbar-left (powerline-wave-right 'tabbar-default nil my/tabbar-height))
+(defvar my/tabbar-right (powerline-wave-left nil 'tabbar-default my/tabbar-height))
+(defun my/tabbar-tab-label-function (tab)
+  (powerline-render (list my/tabbar-left
+                          (format " %s  " (car tab))
+                          my/tabbar-right)))
+(setq tabbar-tab-label-function #'my/tabbar-tab-label-function)
 
   (delete-selection-mode t)
   (fset 'yes-or-no-p 'y-or-n-p)
